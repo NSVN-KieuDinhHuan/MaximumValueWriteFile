045c6f211edf524a409cc2769afcaad9827aeecd
kieuhuan <kieuhuanbka@gmail.com>
2022-01-25 16:20:38 +0700

    first commit

---
+++ src/company/com/main.java
@@ -0,0 +1,27 @@
+package company.com;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile("numbers.txt");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("result.txt", maxValue);
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (max < numbers.get(i)) {
+                max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+}
